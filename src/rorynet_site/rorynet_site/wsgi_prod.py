9253944bf36b88a3f9b72e5f32ba9b75834b4178
Rory Byrne <roryofbyrne@gmail.com>
2017-10-23 16:34:58 +0000

    [Code] Changed tiny name bug

--- src/rorynet_site/rorynet_site/wsgi_prod.py
+++ src/rorynet_site/rorynet_site/wsgi_prod.py
@@ -1,4 +1,4 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_app.settings_prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rorynet_site.settings_prod")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
