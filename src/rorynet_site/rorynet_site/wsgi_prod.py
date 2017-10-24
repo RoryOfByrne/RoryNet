e4a7328282071849a06e3f70b175740560e4ca69
Rory Byrne <roryofbyrne@gmail.com>
2017-10-24 17:00:57 +0000

    WSGI_prog change

--- src/rorynet_site/rorynet_site/wsgi_prod.py
+++ src/rorynet_site/rorynet_site/wsgi_prod.py
@@ -1,7 +1,8 @@
 import os
 import sys
-sys.path.append('/home/rorybyrne/webapps/rorynet/rorynet/src/rorynet_site/rorynet_site')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rorynet_site.settings_prod")
+
+sys.path.append('/home/rorybyrne/webapps/rorynet/')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rorynet_site.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
