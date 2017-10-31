45d5fb58a446c71b61426ff5155b43f4f4605df0
Rory Byrne <roryofbyrne@gmail.com>
2017-10-31 17:37:22 +0000

    [Conf] Added prod settings to gitignore, added neural notwork image

--- src/rorynet_site/rorynet_site/wsgi_prod.py
+++ src/rorynet_site/rorynet_site/wsgi_prod.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-sys.path.append('/home/rorybyrne/webapps/rorynet/')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rorynet_site.settings")
+# sys.path.append('/home/rorybyrne/webapps/rorynet/rorynet/src/')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rorynet_site.settings_prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
