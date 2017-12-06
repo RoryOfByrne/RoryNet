ad3f165c4f13abdb3c646c8862beee77b12834f6
Rory Byrne <roryofbyrne@gmail.com>
2017-12-06 14:56:42 +0000

    [Code] Blog working fully

--- src/rorynet_site/blog/views.py
+++ src/rorynet_site/blog/views.py
@@ -5,17 +5,22 @@ from .models import Blog, Category
 
 class BlogHomeView(TemplateView):
     def get(self, request, **kwargs):
-        blog_posts = Blog.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        blog_posts = Blog.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
         categories = Category.objects.all()
         return render(request, 'blog_home.html', {'blog_posts' : blog_posts, 'categories' : categories})
 
 class BlogPostView(TemplateView):
     def get(self, request, **kwargs):
         blog_post = get_object_or_404(Blog, slug=kwargs['slug'])
-        return render(request, 'blog_post.html', {'blog_post' : blog_post})
+        print(blog_post)
+        categories = Category.objects.all()
+        return render(request, 'blog_post.html', {'blog_post' : blog_post, 'categories' : categories})
 
 class CategoryView(TemplateView):
     def get(self, request, **kwargs):
         category = get_object_or_404(Category, slug=kwargs['slug'])
-        return render(request, 'categories.html',
-                      {'category' : category, 'blog_posts' : Blog.objects.filter(category=category)})
+        categories = Category.objects.all()
+        return render(request, 'blog_category.html',
+                      {'category' : category,
+                       'blog_posts' : Blog.objects.filter(category=category),
+                       'categories' : categories})
